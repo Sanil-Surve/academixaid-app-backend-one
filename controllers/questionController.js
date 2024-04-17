4fb09672cbc279e99585da19e56e0ca3e368f51d
Sanil-Surve <sanilsurve42@gmail.com>
2024-04-17 12:16:22 +0530

    initial commit

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -34,6 +34,24 @@ async function submitQuestion(req, res) {
   }
 }
 
+const getAnswerFromDatabase = async (req, res) => {
+  try {
+    const { question } = req.body;
+    const result = await Question.findOne({ question });
+
+    if (!result) {
+      res.status(404).json({ error: "Answer not found" });
+      return;
+    }
+    const answer = result.answer;
+    res.json({ answer });
+  } catch (error) {
+    console.error("Error fetching answer from database:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
 module.exports = {
   submitQuestion,
+  getAnswerFromDatabase,
 };
--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const questionController = require('../controllers/questionController');
 
-router.post('/api/questions', questionController.submitQuestion);
+router.post("/api/questions", questionController.submitQuestion);
+router.post("/api/fetch-answer", questionController.getAnswerFromDatabase);
 
 module.exports = router;
