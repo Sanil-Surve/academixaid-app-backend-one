fb8338591923152196acaada41194263b0eec107
Sanil-Surve <sanilsurve42@gmail.com>
2024-08-10 21:46:21 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
-// const express = require("express");
-// const bodyparser = require("body-parser");
-// const dotenv = require("dotenv");
-// dotenv.config();
-// require("./models/db");
-// const cors = require("cors");
-
-// const PORT_URI = process.env.PORT || 4000;
-// const app = express();
-// const userRouter = require("./routes/user");
-// const questionRoutes = require("./routes/questionRoutes");
-
-// app.use(express.json());
-// app.use(bodyparser.json());
-// app.use(cors());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.use(userRouter);
-// app.use(questionRoutes);
-
-// app.listen(PORT_URI, () => {
-//   console.log(`Server listening on port:${PORT_URI}`);
-// });
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
--- models/db.js
+++ models/db.js
@@ -1,11 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.set('strictQuery', true);
+mongoose.set('strictQuery', 'true');
 
 const url = process.env.MONGO_URI;
 
-mongoose.connect(url)
-.then(() => {
-  console.log('Database connected');
-})
-.catch(err => console.log(err.message));
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(url);
+    console.log('Database connected');
+  } catch (err) {
+    console.error('Database connection error:', err.message);
+  }
+};
+
+connectToDatabase();
